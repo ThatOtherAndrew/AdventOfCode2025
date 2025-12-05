d64ef0ec42681afa2f2e4e2bfc97044933b70e00
Andromeda <stroev.andrew@gmail.com>
2025-12-05 05:28:08 +0000

    Comment Part 1 solution

--- day5/part1.py
+++ day5/part1.py
@@ -1,7 +1,14 @@
 def main():
     upper, lower = open('input.txt').read().split('\n\n')
-    ranges = [range(int(line.split('-')[0]), int(line.split('-')[1]) + 1) for line in upper.splitlines()]
+    ranges = [
+        range(
+            int(line.split('-')[0]),  # first number
+            int(line.split('-')[1]) + 1  # second number (+1 bc python ranges are semi-inclusive)
+        )
+        for line in upper.splitlines()
+    ]
     ingredients = list(map(int, lower.split()))
+
     print(sum(map(lambda n: any(n in r for r in ranges), ingredients)))
 
 
