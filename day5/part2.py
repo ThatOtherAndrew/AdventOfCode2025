038f1d9a91f93b450ce3892507a8a491887f11cb
Andromeda <stroev.andrew@gmail.com>
2025-12-05 05:38:11 +0000

    Clean up Part 2 solution

--- day5/part2.py
+++ day5/part2.py
@@ -1,16 +1,17 @@
-import functools
-
 import portion
 
 
 def main():
     upper, lower = open('input.txt').read().split('\n\n')
-    ranges = [portion.closed(int(line.split('-')[0]), int(line.split('-')[1])) for line in upper.splitlines()]
+    fresh_ranges = portion.empty()
+    for line in upper.splitlines():
+        # union of ranges handles merging of overlapping & adjacent ranges
+        fresh_ranges |= portion.closed(*map(int, line.split('-')))
 
-    fresh = functools.reduce(lambda a, b: a | b, ranges)
-    print(fresh)
-    print(sum(r.upper - r.lower + 1 for r in fresh))
-
+    print(sum(
+        section.upper - section.lower + 1  # +1 to count upper/lower bounds inclusively
+        for section in fresh_ranges
+    ))
 
 
 if __name__ == '__main__':
