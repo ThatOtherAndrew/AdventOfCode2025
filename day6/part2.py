0c763e05962c4fd58798a37ac2a77eaef0516a43
Andromeda <stroev.andrew@gmail.com>
2025-12-06 05:56:18 +0000

    Day 6 Part 2

---
+++ day6/part2.py
@@ -0,0 +1,20 @@
+def main():
+    raw_lines = open('.input.txt').readlines()
+    total = 0
+    buffer = []
+
+    for column in reversed(list(zip(*raw_lines))):
+        joined = ''.join(column).strip()
+        if not joined:
+            continue  # skip blank columns
+
+        buffer.append(joined)
+        if joined[-1] in '+*': # last character is operator -> compute maths
+            expression = buffer[-1][-1].join(buffer)[:-1]
+            total += eval(expression)
+            buffer = []
+
+    print(total)
+
+if __name__ == '__main__':
+    main()
