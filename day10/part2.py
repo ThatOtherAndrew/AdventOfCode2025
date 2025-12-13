2570adb2ae3f0802844ee43e04181a35239c37e0
Andromeda <stroev.andrew@gmail.com>
2025-12-13 03:16:57 +0000

    nah this is a dead end too

--- day10/part2.py
+++ day10/part2.py
@@ -1,7 +1,7 @@
 from collections import defaultdict
 from collections.abc import Generator, Iterable
 from functools import cache
-from itertools import islice
+from itertools import combinations, product
 
 
 @cache
@@ -11,35 +11,73 @@ def coin_change(target: int, coins: tuple[int], max_coins: int) -> list[list[int
     if not coins:
         return []
 
-    splits = []
-    for count in range(min(target // coins[0], max_coins) + 1):
+    coin_counts = []
+    for count in range(min(target // coins[0], max_coins), -1, -1):
         for counts in coin_change(target - count * coins[0], coins[1:], max_coins - count):
-            splits.append([count, *counts])
-    return splits
+            coin_counts.append([count, *counts])
+    return coin_counts
 
 
-def sorted_coin_change(target: int, coins: Iterable[int]) -> Generator[list[int]]:
-    sorted_coins = tuple(sorted(coins, reverse=True))
-    for max_coins in range(target // min(sorted_coins) + 1):
-        # print(str(max_coins), end='\r')
-        yield from coin_change(target, sorted_coins, max_coins)
+def sorted_coin_change(target: int, coins: tuple[int]) -> Generator[dict[int, int]]:
+    for max_coins in range(target // min(coins) + 1):
+        for counts in coin_change(target, coins, max_coins):
+            yield {coins[i]: count for i, count in enumerate(counts)}
+
+
+@cache
+def partitions(n: int, k: int):
+    # https://stackoverflow.com/questions/28965734/general-bars-and-stars
+    for c in combinations(range(n + k - 1), k - 1):
+        yield [b - a - 1 for a, b in zip((-1,) + c, c + (n + k - 1,))]
+
+
+def find_least_presses(
+    buttons: dict[int, list[tuple[int, ...]]],
+    sizes: tuple[int],
+    button_presses: Iterable[dict[int, int]],
+    starting_state: tuple[int, ...]
+) -> int:
+    for presses in button_presses:
+        button_options = {size: list(partitions(count, len(buttons[size]))) for size, count in presses.items()}
+        print(*map(len, button_options.values()))
+
+        for choices in product(*button_options.values()):
+            state = list(starting_state)
+            skip = False
+
+            for i, size_choices in enumerate(choices):
+                for j, size_presses in enumerate(size_choices):
+                    button = buttons[sizes[i]][j]
+                    for wire in button:
+                        state[wire] -= size_presses
+
+                        # gods this is ugly
+                        if state[wire] < 0:
+                            skip = True
+                            break
+                    if skip: break
+                if skip: break
+
+            if not any(state):
+                return sum(map(sum, choices))
+
+    raise RuntimeError('no solution found')
 
 
 def main():
-    machines: list[tuple[list[int], dict[int, list[tuple[int, ...]]]]] = []
-    for line in open('.input.txt'):
-        *buttons, state = [list(map(int, part[1:-1].split(','))) for part in line.split()[1:]]
+    machines = []
+    for line in open('input.txt'):
+        *buttons, state = [tuple(map(int, part[1:-1].split(','))) for part in line.split()[1:]]
         buttons_dict = defaultdict(list)
-        for button in buttons:
+        for button in sorted(buttons, key=len, reverse=True):
             buttons_dict[len(button)].append(button)
         machines.append((state, buttons_dict))
 
     for state, buttons in machines:
-        print(
-            sum(state),
-            tuple(buttons.keys()),
-            [*islice(sorted_coin_change(sum(state), buttons.keys()), 100)]
-        )
+        print(state, buttons)
+        sizes = tuple(buttons.keys())
+        button_presses = sorted_coin_change(sum(state), sizes)
+        print(find_least_presses(buttons, sizes, button_presses, state))
 
 
 if __name__ == '__main__':
